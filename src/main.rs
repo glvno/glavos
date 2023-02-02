4db21c1ea465bf92b850840e5ecc92a1e3c94a56
Michael Glaviano <michael.glaviano@gmail.com>
2023-02-01 23:11:25 -0600

    handle interrupts, dbg stuff

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,42 @@
-// src/main.rs
+// in src/main.rs
 
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(glavos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use glavos::println;
 
-/// This function is called on panic.
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    println!("Hello World{}", "!");
+
+    glavos::init();
+    x86_64::instructions::interrupts::int3();
+    fn stack_overflow() {
+        stack_overflow();
+    }
+    stack_overflow();
+
+    #[cfg(test)]
+    test_main();
+
+    println!("It did not crash!");
     loop {}
 }
 
-#[no_mangle] // don't mangle the name of this function
-pub extern "C" fn _start() -> ! {
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
+/// This function is called on panic.
+#[cfg(not(test))]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    glavos::test_panic_handler(info)
 }
